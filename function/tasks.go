e7b1d3a429d6df31c25803e1fa4f88a52abe9010
Elizarspbspb <elizarspb@yandex.ru>
2023-06-10 18:01:06 +0300

    Finished tasks for studieng Functions

--- function/tasks.go
+++ function/tasks.go
@@ -41,6 +41,21 @@ func double_m(num_1, num_2 int)(int) {
 	}
 }
 
+func one_or_two(num_1 int, num_2 int, str string) (int, string) {
+	if (str == "one") {
+		return num_1, str
+	} else if (str == "two") {
+		return num_2, str
+	} else {
+		return 0, str
+	}
+}
+
+func mars_age(num_1 int) (int) {
+	num_1 = num_1 * 365 / 687
+	return num_1
+}
+
 func main() {
 	// Task 1: write function max() for 2 integer and return 1 max integer
 	fmt.Println("-------- Task 1 --------")
@@ -64,4 +79,12 @@ func main() {
 	// Task 4: write function double_m() for 2 integer and return sum number*number
 	fmt.Println("-------- Task 4 --------")
 	fmt.Println(double_m(num_1, num_2))
+
+	// Task 5: write function one_or_two() for 2 integer and 1 string (one or two) and return string and 1 num if one , 2 num if two , 0 if not one and not two
+	fmt.Println("-------- Task 5 --------")
+	fmt.Println(one_or_two(num_1, num_2, "one"))
+
+	// Task 6: write function mars_age() for 1 integer Age on Earth and return 1 integer Age on Mars
+	fmt.Println("-------- Task 6 --------")
+	fmt.Println(mars_age(num_1))
 } 
