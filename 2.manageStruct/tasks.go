489afff65b63add8ec4a6702583ef573a2e8ae0a
Elizarspbspb <elizarspb@yandex.ru>
2023-07-22 01:17:38 +0300

    Added information about if..else and requrce

--- 2.manageStruct/tasks.go
+++ 2.manageStruct/tasks.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"math"
+)
 
 func main() {
 	// Task 1: input and output word
@@ -76,5 +78,91 @@ func main() {
 			default:
 
 		}
+	}
+
+	// Task 6: По данному трехзначному числу определить, все ли его цифры различны
+	fmt.Println("-------- Task 6 --------")
+	var number___1 string
+	fmt.Scan(&number___1)
+	if ((number___1[0] == number___1[1]) || (number___1[1] == number___1[2]) || (number___1[0] == number___1[2])) {
+		fmt.Println("NO")
+	} else {
+		fmt.Println("YES")
+	}
+
+	// Task 7: Вывести первую цифру числа
+	fmt.Println("-------- Task 7 --------")
+	// first variant
+	var input_number_str string
+	fmt.Scan(&input_number_str)
+	fmt.Println(input_number_str[0]-48)
+	fmt.Println(string(input_number_str[0]))
+	// second variant with recursion
+	var input_number_int int
+	fmt.Scan(&input_number_int)
+	fmt.Println(getFirstNumber(input_number_int))
+	// first variant with if...else
+	if input_number_int <=9 {
+        fmt.Println(input_number_int)
+    } else if input_number_int >=10 && input_number_int <=99 {
+        fmt.Println(input_number_int/10)
+    } else if input_number_int >=100 && input_number_int <=999 {
+        fmt.Println(input_number_int/100)
+    } else if input_number_int >=1000 && input_number_int <=9999 {    
+        fmt.Println(input_number_int/1000)
+    } else {fmt.Println(input_number_int/10000)}   
+	// fourth variant with math 
+	var input_number_float float64
+	fmt.Scan(&input_number_float)
+	fmt.Println(int(input_number_float / math.Pow10(int(math.Log10(input_number_float)))))
+
+	// Task 8: В шестизначном номере сумма первых трёх цифр совпадает с суммой трёх последних Да или Нет
+	fmt.Println("-------- Task 8 --------")
+	// first variant
+	var happy_ticket string
+	fmt.Scan(&happy_ticket)
+	if((happy_ticket[0] + happy_ticket[1] + happy_ticket[2]) == (happy_ticket[3] + happy_ticket[4] + happy_ticket[5])){
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+	// second variant
+	var happy_ticket_int  int
+	fmt.Scan(&happy_ticket_int)
+	if(getSumNumber(happy_ticket_int/1000) == getSumNumber(happy_ticket_int%1000)){
+		fmt.Println("YES", getSumNumber(happy_ticket_int/1000), "=", getSumNumber(happy_ticket_int%1000))
+	} else {
+		fmt.Println("NO")
+	}
+
+	// Task 9: Високосный год Да или Нет. Кратен 400, или кратен 4 но не кратен 100
+	fmt.Println("-------- Task 9 --------")
+	var year int
+	fmt.Scan(&year)
+	if(((year%400)==0) || ((year%4)==0 && (year%100)!=0)){
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
+
+// Task 7: Вывести первую цифру числа (РЕКУРСИЯ)
+func getFirstNumber(a int) int {
+	if a < 10 {
+		return a
+	} else {
+	 	return getFirstNumber(a / 10)
+	}
+}
+
+// Task 8: Вывести сумму 3 цифр в числе
+func getSumNumber(a int) int {
+	var ost int
+	ost = a % 10
+	fmt.Println("ost =", ost)
+	if a < 10 {
+		return a
+	} else {
+	 	return (ost + getSumNumber(a/10))
 	}
 }
