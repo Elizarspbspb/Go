79b5b65e921400a5f0470961a1966d768b8c7858
Elizarspbspb <elizarspb@yandex.ru>
2023-08-03 23:54:00 +0300

    Added some informations about slices

--- 4.arrays/slices.go
+++ 4.arrays/slices.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// длина (len) — это количество элементов среза;
+// емкость (cap) - количество элементов между началом среза и концом базового массива.
+
 func main () {
 	array := [5]int{-1, 2, 4, 6, 8}
 
@@ -26,6 +29,7 @@ func main () {
 	fmt.Print("\n")
 
 	// dynamic array
+	// make([]T, length, capacity)
 	dyn_array_five := make([]int, 5)
 	dyn_array_five = append(dyn_array_five, 8, 16) 	// insert at the end
 	fmt.Println(dyn_array_five) 					//  [0 0 0 0 0 8 16]
@@ -45,4 +49,38 @@ func main () {
   		fmt.Printf("%c", w) 	// hello
 	}
 	fmt.Print("\n")
+
+	sl_1 := make([]int, 10) // слайс ёмкостью (capacity) 10 и длиной (length) 10, заполненный нулями - [0 0 0 0 0 0 0 0 0 0]. 
+	// И при добавлении элементов в слайс, будут добавляться после последнего 0, т.е. начиная с индекса 10. Повторное выделение памяти
+	fmt.Println(sl_1)	// [0 0 0 0 0 0 0 0 0 0]
+	sl_1 = append(sl_1, 1)
+	fmt.Println(sl_1)	// [0 0 0 0 0 0 0 0 0 0 1] 
+
+	sl_2 := make([]int, 0, 10) // будет создан слайс ёмкостью 10, но не содержащий ни одного элемента (длиной 0) - []. 
+	// Добавление элементов в этот слайс начнётся с 0-го индекса. Повторное выделение памяти если превысит 10
+	fmt.Println(sl_2)	// []
+	sl_2 = append(sl_2, 1)
+	fmt.Println(sl_2)	// [1] 
+
+	sl_3 := make([]int, 5, 10)
+	// будет создан слайс ёмкостью 10, но лежать в нём будут только 5 нулей (длина = 5)
+	fmt.Println(sl_3)	// [0 0 0 0 0] 
+
+	initialUsers := [8]string{"Bob", "Alice", "Kate", "Sam", "Tom", "Paul", "Mike", "Robert"} // базовый массив
+
+	users1 := initialUsers[2:6] 	// с 3-го по 6-й
+	users2 := initialUsers[:4] 		// с 1-го по 4-й
+	users3 := initialUsers[3:] 		// с 4-го до конца
+
+	fmt.Println(users1) 		// [Kate Sam Tom Paul]
+	fmt.Println(users2) 	// [Bob Alice Kate Sam]
+	fmt.Println(users3) 	// [Sam Tom Paul Mike Robert]
+
+	fmt.Printf(
+		"Срез, основанный на базовом массиве длиной %d и емкостью %d: %v\n", 
+		len(users1), 	// 4
+		cap(users1),	// 6
+		users1,			// [Kate Sam Tom Paul]
+	)
+	pointer := fmt.Sprintf("%p", users1)
 }
